902d625247597f123749a8235c44e147d4e477de
Pedro Soares <pedro.silva@atlasinovacoes.com.br>
2025-10-02 16:28:40 -0300

    Adição de uma opção gráfica das peças

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -7,9 +7,27 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; //Quero dar a opção do usuário usar os símbolos ao invés das letras, o jogo fica mais legal
+
+            Console.WriteLine("Simbolos gráficos: ♔, ♕, ♖, ♗, ♘, ♙\n" +
+                "Letras: K, Q, R, B, N, P");
+            Console.WriteLine();
+            Console.WriteLine("Usar símbolos gráficos de xadrez? (s/n)");
+            string option = Console.ReadLine().ToLower();
+
+            bool usarSimbolos = option == "s";
+
+            string rei = usarSimbolos ? "♔" : "K";
+            string rainha = usarSimbolos ? "♕" : "Q";
+            string torre = usarSimbolos ? "♖" : "R";
+            string bispo = usarSimbolos ? "♗" : "B";
+            string cavalo = usarSimbolos ? "♘" : "N";
+            string peao = usarSimbolos ? "♙" : "P";
+
+            Console.WriteLine($"Exemplo: {rei} {rainha} {torre} {bispo} {cavalo} {peao}");
+
             Tabuleiro tab = new Tabuleiro (8,8);
 
-            Console.ReadLine();
         }
     }
 }
